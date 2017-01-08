dfc22437f2d5713d5f96b8d4e3b5b91d3e4095a4
jingtingzeng <kelly20829tw@gmail.com>
2017-01-09 00:31:19 +0800

    fix bug

--- server.js
+++ server.js
@@ -254,12 +254,9 @@ io.on('connection',function(socket){
  	socket.on('add user',function(msg){
  		socket.username=msg;
  		user.name = socket.username;                    // 接收user name
-      	users.push(user); 
+      	users.push(socket.username); 
  		console.log("new user:"+msg+"logged.");
- 		io.emit('add user',{
- 			username:socket.username
- 		});
- 		users.push(msg);//在線上的
+ 		io.emit('usernames',users);
  	});
  	//監聽新訊息事件
  	//群聊
